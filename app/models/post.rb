9efac984437630db8b86bb89335e6e6d40f9b9a0
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-09-18 10:51:52 +0700

    add processing data

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  def update_comments_counter
+    update(comment_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(like_counter: likes.count)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_posts_counter
+    update(post_counter: posts.count)
+  end
+
 end
