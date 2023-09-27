ba850ab37f0a60e56f9b58dca70a5a8ab49284c5
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-09-27 22:23:41 +0700

    user view test

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,8 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe 'users/show.html.erb', type: :view do
-  it 'displays the expected content' do
-    render
-    expect(rendered).to have_content('This is user per id')
+RSpec.feature 'User Show Page' do
+  # Create a test user with posts
+  let!(:user1) { User.create(name: 'user1', bio: 'this is bio', photo: 'user1.jpg') }
+  let!(:post1) { Post.create(author: user1, title: 'Post 1', text: 'This is post 1') }
+  let!(:post2) { Post.create(author: user1, title: 'Post 2', text: 'This is post 2') }
+  let!(:post3) { Post.create(author: user1, title: 'Post 3', text: 'This is post 3') }
+  let!(:post4) { Post.create(author: user1, title: 'Post 4', text: 'This is post 4') }
+
+  scenario 'Displays user profile information' do
+    visit user_path(user1)
+
+    # Verify that the user's profile picture is displayed
+    expect(page).to have_css("img[src*='#{user1.photo}']")
+
+    # Verify that the user's username is displayed
+    expect(page).to have_content(user1.name)
+
+    # Verify that the number of posts the user has written is displayed
+    expect(page).to have_content("Number of post : #{user1.post_counter}")
+
+    # Verify that the user's bio is displayed
+    expect(page).to have_content(user1.bio)
+  end
+
+  scenario 'Displays the first 3 posts' do
+    visit user_path(user1)
+
+    # Verify that the titles of the first 3 posts are displayed
+    expect(page).to have_content('Post 2')
+    expect(page).to have_content('Post 3')
+    expect(page).to have_content('Post 4')
+
+    # Verify that the content of the first 3 posts is displayed
+    expect(page).to have_content('This is post 2')
+    expect(page).to have_content('This is post 3')
+    expect(page).to have_content('This is post 4')
+  end
+
+  scenario 'Redirects to the post show page when a post is clicked' do
+    visit user_path(user1)
+
+    # Click on a post
+    click_link post2.title
+
+    # Verify that the page redirects to the post's show page
+    expect(current_path).to eq(user_post_path(user_id: post2.author_id, id: post2.id))
+  end
+
+  scenario 'Redirects to the user\'s post index page when "Load all post" is clicked' do
+    visit user_path(user1)
+
+    click_link 'Load all post'
+
+    # Verify that the page redirects to the user's post index page
+    expect(current_path).to eq(user_posts_path(user1))
   end
 end
