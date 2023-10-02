a322579dcf9f9d8eda19893437bf93c2ff349ec5
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-02 15:27:34 +0700

    update request test for post and user

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /users/:user_id/posts/:id' do
     it 'returns http success' do
-      get '/users/1/posts/1'
+      @user = User.create(name: 'Ichsan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Web Developer')
+      @post = Post.create(title: 'test', author_id: @user.id, text: 'this is testing')
+      get user_post_path(user_id: @user.id, id: @post.id)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /users/:user_id/posts' do
     it 'returns http success' do
-      get '/users/1/posts'
+      @user = User.create(name: 'Ichsan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Web Developer')
+      get user_posts_path(@user.id)
       expect(response).to have_http_status(:success)
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /users/:id' do
     it 'returns http success' do
-      get '/users/1'
+      @user = User.create(name: 'Ichsan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Web Developer')
+      get user_path(@user.id)
       expect(response).to have_http_status(:success)
     end
   end
