0e47b8c6b025d92749fa13134946f021b3e52118
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-02 18:57:23 +0700

    create another block for user test

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -7,19 +7,29 @@ RSpec.feature 'User Index Page' do
   let!(:post1) { Post.create(author: user1) }
   let!(:post2) { Post.create(author: user1) }
 
-  scenario 'Displays usernames, profile pictures, and post counts' do
+  scenario 'Displays user1 usernames, profile pictures, and post counts' do
     visit users_path
 
     # Verify that usernames are displayed
     expect(page).to have_content(user1.name)
-    expect(page).to have_content(user2.name)
 
     # Verify that profile pictures are displayed (assuming you have an 'avatar' attribute)
     expect(page).to have_css("img[src*='#{user1.photo}']")
-    expect(page).to have_css("img[src*='#{user2.photo}']")
 
     # Verify that post counts are displayed
     expect(page).to have_content("Number of post : #{user1.post_counter}")
+  end
+
+  scenario 'Displays user2 usernames, profile pictures, and post counts' do
+    visit users_path
+
+    # Verify that usernames are displayed
+    expect(page).to have_content(user2.name)
+
+    # Verify that profile pictures are displayed (assuming you have an 'avatar' attribute)
+    expect(page).to have_css("img[src*='#{user2.photo}']")
+
+    # Verify that post counts are displayed
     expect(page).to have_content("Number of post : #{user2.post_counter}")
   end
 
