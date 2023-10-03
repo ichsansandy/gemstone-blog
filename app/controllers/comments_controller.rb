8ee3b25e1655992323c785076002ca8a70ccdeb8
kessie <kayp3477@gmail.com>
2023-10-03 15:33:08 +0000

    Add destroy method in to decrement comment_counter and ensure authorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,15 @@ class CommentsController < ApplicationController
     redirect_to user_post_path(current_user, @post)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    authorize! :destroy, @comment # Ensure user is authorized to delete the comment
+    @post.decrement!(:comment_counter)
+    @comment.destroy
+    redirect_to user_post_path(params[:user_id], params[:post_id]), notice: "Comment was successfully deleted."
+  end
+
   private
 
   def comment_params
