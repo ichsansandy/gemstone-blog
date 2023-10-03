938849e7b169b257c14dc4db0eaf2325e312ad05
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-04 06:07:21 +0700

    remove trailling space

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
-  
+
   def create
     @post = Post.find(params[:id])
     @comment = @post.comments.new(comment_params)
